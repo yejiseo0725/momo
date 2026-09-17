d21269480230a6fcec5b60577ad82bf73788a6e8
hjkim <h1j777777@gmail.com>
2026-09-17 10:28:36 +0900

    feat: 모임 카드에 생성자 표시

--- components/GatheringCard.js
+++ components/GatheringCard.js
@@ -17,6 +17,8 @@ export default function GatheringCard({ gathering, showLeader = false }) {
       <p>{gathering.description}</p>
       <footer>
         <small>
+          만든 사람: {gathering.creatorName}
+          <br />
           {gathering.memberCount} / {gathering.maxMemCount}명
           {gathering.isPublic ? " · 공개" : " · 비공개"}
         </small>
